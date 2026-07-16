6f9ad765a2d1b4ff45b7646d2834dbda37f26f46
icaroandrade45-create <icaroandrade45@gmail.com>
2026-07-16 10:12:16 +0000

    Corrige definicao da variavel global supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configuração do Supabase v2
-const SUPABASE_URL = "https://ukxylcyenryhzvjlzyaz.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhYmdjOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // Mantém a tua chave real completa aqui
+const SUPABASE_URL =" "https://ukxylcyenryhzvjlzyaz.supabase.co";
+const SUPABASE_ANON_KEY =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVreHlsY3llbnJ5aHp2amx6eWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MzEyOTksImV4cCI6MjA5OTIwNzI5OX0.wpU9LQscvrCFqK5vBDrL1nlhZMYer5DA-F6vWyamt6I "m"; // Garante que a tua chave real está aqui completa
 
-// Criamos o cliente e guardamos diretamente no objeto global 'window'
-window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Inicializa o cliente numa variável global direta
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
