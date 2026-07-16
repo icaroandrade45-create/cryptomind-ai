30f440fae8e4763776366bcab9b5da609979bb5e
icaroandrade45-create <icaroandrade45@gmail.com>
2026-07-16 11:58:37 +0000

    Declara variavel global supabase usando var

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
-// Configuração do Supabase v2
+// Configuração do Supabase
 const SUPABASE_URL = "https://ukxylcyenryhzvjlzyaz.supabase.co";
-const SUPABASE_ANON_KEY "=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVreHlsY3llbnJ5aHp2amx6eWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MzEyOTksImV4cCI6MjA5OTIwNzI5OX0.wpU9LQscvrCFqK5vBDrL1nlhZMYer5DA-F6vWyamt6I "; // Coloca aqui a tua chave real do Supabase sem aspas duplicadas
+const SUPABASE_ANON_KEY = "eyJhYmdjOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // Coloca aquieyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVreHlsY3llbnJ5aHp2amx6eWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MzEyOTksImV4cCI6MjA5OTIwNzI5OX0.wpU9LQscvrCFqK5vBDrL1nlhZMYer5DA-F6vWyamt6I a tua chave real completa
 
-// Inicializa o cliente na variável global window.supabase
-if (window.supabase && typeof window.supabase.createClient === 'function') {
-    window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-    } else {
-        console.error("Erro: A biblioteca do Supabase não foi carregada no window.");
-        }
+// Esta linha garante a criação de uma variável global direta chamada 'supabase'
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
