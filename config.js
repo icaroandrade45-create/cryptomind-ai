4574f2e7e2285db6babd7d7c8423f74cbbacecb3
icaroandrade45-create <icaroandrade45@gmail.com>
2026-07-16 14:56:17 +0000

    Codigo totalmente estruturado e corrigido com alertas

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const SUPABASE_URL = "https://ukxylcyenryhzvjlzyaz.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhYmdjOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVreHlsY3llbnJ5aHp2amx6eWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU5MDU2OTIsImV4cCI6MjAzMTQ4MTY5Mn0.88V_W6RkHl9Lz7f0HlS3Z8S87pL-8vR6S7_76_y-9sE";
 
-// Criamos o cliente numa variável global chamada 'supabaseCliente' para não quebrar a biblioteca
+// Criamos o cliente numa variável global chamada 'supabaseCliente' para evitar conflitos
 window.supabaseCliente = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
