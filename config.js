580ca9852285cc7106140e6538371dbe65743bdf
icaroandrade45-create <icaroandrade45@gmail.com>
2026-07-18 14:33:56 +0000

    aplica a url e chave anon fornecidas

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Configuração do Supabase (URL e Chave Atualizadas e Sincronizadas)
+// Configuração do Supabase (URL e Chave Sincronizadas)
 const SUPABASE_URL = "https://ukxylcyenryhzvjlzyaz.supabase.co";
 const SUPABASE_ANON_KEY = "EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVreHlsY3llbnJ5aHp2amx6eWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MzEyOTksImV4cCI6MjA5OTIwNzI5OX0.wpU9LQscvrCFqK5vBDrL1nlhZMYer5DA-F6vWyamt6I";
 
