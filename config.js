e68a4620397cea7c443df6e3ce4e110407d48dfa
icaroandrade45-create <icaroandrade45@gmail.com>
2026-07-18 02:18:12 +0000

    corrige credenciais e inicializacao do supabase

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+cat << 'EOF' > config.js
 // Configuração do Supabase
 const SUPABASE_URL = "https://ukxylcyernyhzkwhgqkd.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVreHlsY3llbnJ5aHp2amx6eWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MzEyOTksImV4cCI6MjA5OTIwNzI5OX0.wpU9LQscvrCFqK5vBDrL1nlhZMYer5DA-F6vWyamt6I";
@@ -6,3 +7,4 @@ window.supabaseCliente = window.supabase.createClient(
     SUPABASE_URL,
         SUPABASE_ANON_KEY
         );
+        EOF
