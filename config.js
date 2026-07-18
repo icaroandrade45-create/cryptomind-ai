f92f239516250310aca32724e0d98b801e3d198b
icaroandrade45-create <icaroandrade45@gmail.com>
2026-07-18 01:34:10 +0000

    restaura e corrige config.js completo

--- config.js
+++ config.js
@@ -3,8 +3,6 @@ const SUPABASE_URL = "https://ukxylcyernyhzkwhgqkd.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVreHlsY3llbnJ5aHp2amx6eWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MzEyOTksImV4cCI6MjA5OTIwNzI5OX0.wpU9LQscvrCFqK5vBDrL1nlhZMYer5DA-F6vWyamt6I";
 
 window.supabaseCliente = window.supabase.createClient(
-        SUPABASE_URL,
-            SUPABASE_ANON_KEY
-            );
-            
-)
+    SUPABASE_URL,
+        SUPABASE_ANON_KEY
+        );
