f4e1b9da8d953010481fdbf2de40ebc05addd4a2
icaroandrade45-create <icaroandrade45@gmail.com>
2026-07-19 02:37:16 +0000

    fix: atualiza chaves do supabase

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // CryptoMind AI - Configuração Centralizada do Supabase
-const SUPABASE_URL = "https://ukxylcyenryhzvjlzyaz.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVreHlsY3llbnJ5aHp2amx6eWF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MzEyOTksImV4cCI6MjA5OTIwNzI5OX0.wpU9LQscvrCFqK5vBDrL1nlhZMYer5DA-F6vWyamt6I";
+// config.js
+export const SUPABASE_URL = 'https://xrtbtflftnjzzwfozjiy.supabase.co/rest/v1/';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhydGJ0ZmxmdG5qenp3Zm96aml5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NDczMTgsImV4cCI6MjA5OTQyMzMxOH0.YX3Ghb1HyOciVat-dUmh0FxmgpJFrp17Mo_RhuJ5hzQ';
 
 let supabaseInstance = null;
 
